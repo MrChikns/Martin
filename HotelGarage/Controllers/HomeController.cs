d9adb6cbb75dca68c1a5ca09bc460a2d7072ac2a
Martin Severa <severa.martin@gmail.com>
2018-10-11 21:33:24 +0200

    car create from and list of cars on the home page

--- HotelGarage/Controllers/HomeController.cs
+++ HotelGarage/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HotelGarage.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,17 @@ namespace HotelGarage.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var cars = _context.Cars.ToList();
+            return View(cars);
         }
 
         public ActionResult About()

d90c45f8160013544ea109a7f72e4d49522cf811
Martin Severa <severa.martin@gmail.com>
2018-10-17 23:05:12 +0200

    prechod na bootstrap view a pridani parkingplaceDTO

--- HotelGarage/Controllers/ParkingController.cs
+++ HotelGarage/Controllers/ParkingController.cs
@@ -6,6 +6,7 @@ using System.Data.Entity;
 using System.Web;
 using System.Web.Mvc;
 using HotelGarage.ViewModels;
+using HotelGarage.Dtos;
 
 namespace HotelGarage.Controllers
 {
@@ -28,8 +29,49 @@ namespace HotelGarage.Controllers
 
             return View(parkingPlaces);
         }
-                
-       public ActionResult CheckIn(ActualReservation reservation)
+
+        public ActionResult OutsideParking()
+        {
+            IList<ParkingPlace> parkingPlaces = _context.ParkingPlaces
+            .Include(s => s.StateOfPlace)
+            .Include(r => r.Reservation)
+            .Include(c => c.Reservation.Car)
+            .ToList();
+
+            IList<ParkingPlaceDto> parkingPlaceDtos = new List<ParkingPlaceDto>();
+
+            foreach(var parkingPlace in _context.ParkingPlaces)
+            {
+                string lPlate = "", departure = "", name = "";
+
+                string sOPlace = parkingPlace.StateOfPlace.Name;
+
+                if(parkingPlace.Reservation != null)
+                {
+                    lPlate = parkingPlace.Reservation.LicensePlate;
+                    departure = parkingPlace.Reservation.Departure.ToShortDateString();
+                    name = parkingPlace.Name;
+                }
+
+                var ppDto = new ParkingPlaceDto {
+                    LicensePlate = lPlate,
+                    Departure = departure,
+                    Name = name,
+                    StateOfPlace = sOPlace 
+                };
+
+                parkingPlaceDtos.Add(ppDto);
+            }
+
+          //  foreach (var pPlace in parkingPlaces)
+            //{ if (pPlace.Reservation == null)
+              //      pPlace.Reservation = _context.Reservations.Where(i => i.Id == 15);
+                    
+                //        } 
+            return View(parkingPlaceDtos);
+        }
+
+        public ActionResult CheckIn(ActualReservation reservation)
         {
             var parkingPlaces = _context.ParkingPlaces
                 .Include(s => s.StateOfPlace)
---
+++ HotelGarage/Dtos/ParkingPlaceDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HotelGarage.Dtos
+{
+    public class ParkingPlaceDto
+    {
+        public string LicensePlate { get; set; }
+        public string Departure { get; set; }
+        public string Name { get; set; }
+        public string StateOfPlace { get; set; }
+    }
+}

c1ab6337e150702893531ef801af7c0fb7b7e66b
Martin Severa <severa.martin@gmail.com>
2018-11-25 23:12:37 +0100

    js app file refactoring

--- HotelGarage/Scripts/app/app.js
+++ HotelGarage/Scripts/app/app.js
@@ -1,61 +1,95 @@
-﻿
-$(document).ready(function () {
-    //
-    // res list =>
-    //
-    // datatable rezervaci
+﻿//
+// res list =>
+//
+var ReservationList = function () { 
+    var init = function () {
+        table.draw();
+        $("#filterBtn").on("click", function () { table.draw(); });
+    };
+
+    // prepsani datatable s nastavenymi daty a filtrem
     var table = $('#example').DataTable({
-        columnDefs: [
-            {
-                targets: [0, 1, 2],
-                className: 'mdl-data-table__cell--non-numeric'
-            }
-        ]
+        columnDefs: [{
+            targets: [0, 1, 2],
+            className: 'mdl-data-table__cell--non-numeric'
+        }]
     });
 
-    //aktivace filtru z nastavenych dat
-    $("#filterBtn").on("click", function () { table.draw(); });
+    return {
+        init: init
+    };
+}();
 
-    //
-    // res form =>
-    //
+
+
+//
+// res form =>
+//
+var ReservationForm = function () { 
+    var init = function () {
+        setUpBoxColor();
+        $('.js-res-save-btn').on("click", departureAfterArrivalCheck);
+        $('#IsRegistered').on("click", toggleBoxColor);
+        $('input[data-id="spz-visible"]').on('keyup', passLicensePlaceIntoModel);
+    };
+
     // kontrola prijezdu pri vytvareni rezervace - zda je odjezd az po prijezdu
-    $('.js-res-save-btn').on("click", function (event) {
-        var arrival = $('#Arrival').val();
-        var departure = $('#Departure').val();
-
-        if (arrival > departure) {
+    var departureAfterArrivalCheck = function (event) {
+        if ($('#Arrival').val() > $('#Departure').val()) {
             alert("Datum odjezdu musí být po datu příjezdu!");
             event.preventDefault();
-        }
-    });
+        };
+    };
+
+    // prebarveni stavu parkovaciho mista podle stavu registrace hosta
+    var setUpBoxColor = function () {
+        if ($('#IsRegistered').attr('checked') === "checked") { $('#isRegisteredDiv').addClass('alert alert-success'); }
+        else { $('#isRegisteredDiv').addClass('alert alert-warning'); }
+    };
 
     // prepinani barvy pri kliknuti na checkbox
-    $('#IsRegistered').on("click", function () { $('#isRegisteredDiv').toggleClass('alert-warning alert-success'); });
+    var toggleBoxColor = function () { $('#isRegisteredDiv').toggleClass('alert-warning alert-success'); };
 
     // doplneni SPZ z textboxu aby byl vracen kompletni viewModel
-    $('input[data-id="spz-visible"]').on('keyup', function () {
+    var passLicensePlaceIntoModel = function () {
         $('input[data-id="spz-hidden"]').val($(this).val());
-    });
+    };
 
-    //
-    // main parking place =>
-    //
-    // zvyrazneni neprirazeneho mista u najizdejici rezervace
-    $('div.alert-prijezd').each(function () {
-        $(this).children(".js-pPlacePrijezd:contains('Nepřiřazeno')").addClass("alert-link");
-    });
+    return {
+        init: init
+    };
+}();
 
-    // vypnuti checkinu pro neprirazene rezervace
-    $('a.odkaz-prijezd').each(function () {
+
+//
+// main parking place =>
+//
+var ParkingPlace = function () {
+    var init = function () {
+        $('div.alert-prijezd').each(highlightTodaysReservationWithoutPPlaceAssigned);
+        $('a.odkaz-prijezd').each(hideCheckInBtnForReservationWithoutPPlaceAssigned);
+        $('.select-link').on("click", assignReservationIntoParkingPlace);
+        $('div.card-body>a').each(parkingPlaceLinksAndStateSetUp);
+        $(".card-title").each(employeeStateSetUp);
+        $(document).on("click", "a.js-checkout", checkOutModalSetup);
+        $(document).on("click", ".js-sOPChange", parkingPlaceModalSetUp);
+
+        // obnoveni stranky po ajax callu do controlleru
+        $(document).ajaxStop(function () { window.location.reload(); });
+    };
+
+    var highlightTodaysReservationWithoutPPlaceAssigned = function () {
+        $(this).children(".js-pPlacePrijezd:contains('Nepřiřazeno')").addClass("alert-link")
+    };
+
+
+     var hideCheckInBtnForReservationWithoutPPlaceAssigned = function () {
         if ($(this).attr('isregistered') === 'False') {
             $(this).addClass("disabled");
         }
-    });
+    };
 
-    // prirazeni rezervace na parkovaci misto
-    $('.select-link').on("click", function () {
-        //reservation id
+    var assignReservationIntoParkingPlace = function () {
         var vId = $(this).attr('value-id');
         var btnType = $(this).attr('buttonType');
         var resId = $('button[value-id="' + vId + '"][buttonType="' + btnType + '"]').attr('data-id');
@@ -68,91 +102,51 @@ $(document).ready(function () {
             url: "/Parking/Reserve",
             data: { ParkingPlaceName: pPName, ReservationId: resId }
         });
-    });
+    };
 
-    // prepsani stavu parkovaciho mista a skryti nepotrebnych akci pro stav parkovaciho mista
-    $('div.card-body>a').each(function () {
-        if ($(this).text() === 'Volno')
-        {            
-            $(this).addClass('btn btn-success');
-            $(this).next().children(".js-checkout, .js-checkin, .js-upravit").hide();
-        }
+    var parkingPlaceLinksAndStateSetUp = function () {
+        var btnState = function (btn, state, addClasses, hideLinks) {
+            if ($(btn).text() === state) {
+                $(btn).addClass(addClasses);
+                $(btn).next().children(hideLinks).hide();
+            }
+        };
 
-        if ($(this).text() === 'Obsazeno')
-        {
-            $(this).addClass('btn btn-primary');
-            $(this).next().children(".js-checkin, .js-reserve").hide();
-        }
+        var leaveReserve = ".js-checkout, .js-checkin, .js-upravit";
+        var leaveCOutEdit = ".js-checkin, .js-reserve";
+        var leaveEdit = ".js-checkout, .js-checkin, .js-reserve";
+        var leaveCInEdit = ".js-checkout, .js-reserve";
 
-        if ($(this).text() === 'Rezervováno')
-        {
-            $(this).addClass('btn btn-warning');
-            $(this).next().children(".js-checkout, .js-reserve").hide();
-        }
+        btnState(this, 'Volno', 'btn btn-success', leaveReserve);
+        btnState(this, 'Obsazeno', 'btn btn-primary', leaveCOutEdit);
+        btnState(this, 'Rezervováno', 'btn btn-warning', leaveCInEdit);
+        btnState(this, 'Neregistrován!', 'btn btn-secondary', leaveEdit);
+        btnState(this, 'Odjezd', 'btn btn-danger', leaveCOutEdit);
+        btnState(this, 'Volno Staff', 'btn btn-light', leaveReserve);
+    } ;
 
-        if ($(this).text() === 'Neregistrován!')
-        {
-            $(this).addClass('btn btn-secondary');
-            $(this).next().children(".js-checkout, .js-checkin, .js-reserve").hide();
-        }
-
-        if ($(this).text() === 'Odjezd')
-        {
-            $(this).addClass('btn btn-danger');
-            $(this).next().children(".js-checkin, .js-reserve").hide();
-        }
-
-        if ($(this).text() === 'Volno Staff')
-        {
-            $(this).addClass('btn btn-light');
-            $(this).next().children(".js-checkout, .js-checkin, .js-upravit").hide();
-        }
-    });
-
-    // prebarveni stavu parkovaciho mista podle stavu registrace hosta
-    if ($('#IsRegistered').attr('checked') === "checked") { $('#isRegisteredDiv').addClass('alert alert-success'); }
-    else { $('#isRegisteredDiv').addClass('alert alert-warning'); }
-
-    // zmena barvy tlacitek u zamestnancu, aby je bylo mozne oddelit od normalnich hostu
-    $(".card-title").each(function () {
+    var employeeStateSetUp = function () {
         if ($(this).next().attr("data-bbox-zamestnanec") === 'Zaměstnanec'
-            && $(this).next().next().hasClass("btn-primary") === true)
-        {
+            && $(this).next().next().hasClass("btn-primary") === true) {
             $(this).next().next().toggleClass('btn-primary btn-outline-success');
         }
-    });
-    
-    // obnoveni stranky po ajax callu do controlleru
-    $(document).ajaxStop(function () {
-        window.location.reload();
-    });
-    
-    // modal pro potvrzeni checkoutu rezervace
-    $(document).on("click", "a.js-checkout", function (e) {
+    };
+
+    var checkOutModalSetup = function (e) {
         if ($(this).attr('isregistered') === 'False') {
             return false;
         }
         else {
             var id = $(this).parent().attr('Id');
-            var spz;
-            var odjezd;
-
-            if ($(this).hasClass("odkaz-prijezd")) {
-                spz = "SPZ:  " + $(this).attr('data-bbox-spz');
-                odjezd = $(this).parent().prev().prev().attr('data-bbox-odjezd');
-            }
-            else {
-                spz = "SPZ:  " + $(this).parent().prev().prev().attr('data-bbox-spz');
-                odjezd = $(this).parent().prev().prev().attr('data-bbox-odjezd');
-            }
+            var spz = "SPZ:  " + $(this).hasClass("odkaz-prijezd") ? $(this).attr('data-bbox-spz') :
+                $(this).parent().prev().prev().attr('data-bbox-spz');
+            var odjezd = $(this).parent().prev().prev().attr('data-bbox-odjezd');
 
             var msgCheckOut = "<div class=\"row alert alert-primary\"><div class=\"nav-link\">Chcete ukončit pobyt?</div>" +
                 "<a class=\"nav-link js-checkout\" href=\"/Parking/CheckOut?pPlaceId=" + id + "\">Check Out</a></div>";
-
             var msgVyjezd = "<div class=\"row alert alert-primary\"><div class=\"nav-link\">Dočasný výjezd?</div>" +
                 "<a class=\"nav-link js-checkout\" href=\"/Parking/TemporaryLeave?pPlaceId=" + id + "\">Výjezd</a></div>";
 
-            
             var d = new Date();
             var date = d.getDate();
             var month = d.getMonth() + 1;
@@ -167,39 +161,46 @@ $(document).ready(function () {
                 }
             });
         }
-    });
+    };
 
-    // modal window pri kliknuti na stav parkovaciho mista
-    $(document).on("click", ".js-sOPChange", function (e) {
+    var parkingPlaceModalSetUp = function (e) {
 
-        var spz = $(this).prev().attr("data-bbox-spz").replace(/_/g," ");
-        var prijezd = $(this).prev().attr("data-bbox-prijezd").replace(/_/g, " ");
-        var odjezd = $(this).prev().attr("data-bbox-odjezd").replace(/_/g, " ");
-        var pokoj = $(this).prev().attr("data-bbox-pokoj").replace(/_/g, " ");
-        var jmeno = $(this).prev().attr("data-bbox-jmeno").replace(/_/g, " ");
-        var cena = $(this).prev().attr("data-bbox-cena").replace(/_/g, " ");
-        var zamestnanec = $(this).prev().attr("data-bbox-zamestnanec").replace(/_/g, " ");
-        var jeRegistrovan = $(this).prev().attr("data-bbox-jeRegistrovan").replace(/_/g, " ");
-        var typAuta = $(this).prev().attr("data-bbox-typAuta").replace(/_/g, " ");
-        var poznamka = $(this).prev().attr("data-bbox-poznamka").replace(/_/g, " ");
+        var getDataAttr = function (elem, attribute) {
+            return $(elem).prev().attr(attribute);
+        };
+
+        var spz = getDataAttr(this, "data-bbox-spz");
+        var prijezd = getDataAttr(this, "data-bbox-prijezd");
+        var odjezd = getDataAttr(this, "data-bbox-odjezd");
+        var pokoj = getDataAttr(this, "data-bbox-pokoj");
+        var jmeno = getDataAttr(this, "data-bbox-jmeno");
+        var cena = getDataAttr(this, "data-bbox-cena");
+        var zamestnanec = getDataAttr(this, "data-bbox-zamestnanec");
+        var jeRegistrovan = getDataAttr(this, "data-bbox-jeRegistrovan");
+        var typAuta = getDataAttr(this, "data-bbox-typAuta");
+        var poznamka = getDataAttr(this, "data-bbox-poznamka");
 
         var dialog = bootbox.dialog({
             title: spz,
-            message: 
+            message:
                 "<div class= 'container'>" + "<div class=\"row\"><div class=\"col-sm-4\" style=\"text-align:right\">" +
                 "Příjezd: <br> Odjezd: <br>Pokoj: <br>" + zamestnanec +
                 ": <br>Registrován: <br>Cena: <br>Typ Auta: <br>Poznámka: " +
                 "</div ><div class=\"col-sm\">" + prijezd + "<br>" + odjezd + "<br>" + pokoj +
                 "<br>" + jmeno + "<br>" + jeRegistrovan + "<br>" + cena + "<br>" + typAuta + "<br>" +
                 poznamka + "</div ></div ></div > "
-                ,
+            ,
             buttons: {
-                
+
                 ok: {
                     label: "Zavřít",
                     className: 'btn-info'
                 }
             }
         });
-    });
-});
+    };
+
+    return {
+        init: init
+    };
+}();

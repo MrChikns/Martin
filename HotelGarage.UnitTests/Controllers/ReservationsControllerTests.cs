048df58c863fc281564fec07765db2affaa4ddaa
Martin Severa <severa.martin@gmail.com>
2019-04-10 23:46:36 +0200

    reservation controller save method tests

--- HotelGarage.UnitTests/Controllers/ReservationsControllerTests.cs
+++ HotelGarage.UnitTests/Controllers/ReservationsControllerTests.cs
@@ -22,9 +22,11 @@ namespace HotelGarage.UnitTests.Controllers
         Mock<IUnitOfWork> _mockUnitOfWork;
         Mock<IReservationRepository> _mockReservationRepository;
         private Mock<IParkingPlaceRepository> _mockParkingPlaceRepository;
+        private Mock<ICarRepository> _mockCarRepository;
         private Mock<IStateOfPlaceRepository> _mockStatesOfPlacesRepository;
         private Reservation _reservation;
         private ParkingPlace _parkingPlace;
+        private Car _car;
         private int _existing = 1;
         private int _nonExisting = 2;
 
@@ -49,19 +51,27 @@ namespace HotelGarage.UnitTests.Controllers
                 Reservation = _reservation
             };
 
+            _car = new Car();
+
             _mockReservationRepository = new Mock<IReservationRepository>();
+            _mockReservationRepository.Setup(r => r.GetReservation(_existing)).Returns(_reservation);
             _mockReservationRepository.Setup(r => r.GetReservationCar(_existing)).Returns(_reservation);
 
             _mockParkingPlaceRepository = new Mock<IParkingPlaceRepository>();
             _mockParkingPlaceRepository.Setup(p => p.GetParkingPlace(_existing)).Returns(_parkingPlace);
+            _mockParkingPlaceRepository.Setup(p => p.GetParkingPlaceStateOfPlace(_reservation)).Returns(_parkingPlace);
+
+            _mockCarRepository = new Mock<ICarRepository>();
+            _mockCarRepository.Setup(c => c.GetCar(_reservation)).Returns(_car);
 
             _mockStatesOfPlacesRepository = new Mock<IStateOfPlaceRepository>();
             _mockStatesOfPlacesRepository.Setup(s => s.GetFreeStateOfPlace()).Returns(new StateOfPlace());
-            //_mockStatesOfPlacesRepository.Setup(s => s.GetOccupiedStateOfPlace()).Returns(new StateOfPlace());
+            _mockStatesOfPlacesRepository.Setup(s => s.GetReservedStateOfPlace()).Returns(new StateOfPlace());
 
             _mockUnitOfWork = new Mock<IUnitOfWork>();
             _mockUnitOfWork.SetupGet(u => u.Reservations).Returns(_mockReservationRepository.Object);
             _mockUnitOfWork.SetupGet(u => u.ParkingPlaces).Returns(_mockParkingPlaceRepository.Object);
+            _mockUnitOfWork.SetupGet(u => u.Cars).Returns(_mockCarRepository.Object);
             _mockUnitOfWork.SetupGet(u => u.StatesOfPlaces).Returns(_mockStatesOfPlacesRepository.Object);
 
             _controller = new ReservationsController(_mockUnitOfWork.Object);
@@ -75,7 +85,7 @@ namespace HotelGarage.UnitTests.Controllers
         }
 
         [Test]
-        public void Update_ReservationIdExists_ReturnsView()
+        public void Update_ReservationIdExists_ReturnView()
         {
             var result = _controller.Update(_existing) as ViewResult;
 
@@ -90,7 +100,7 @@ namespace HotelGarage.UnitTests.Controllers
         }
 
         [Test]
-        public void Delete_ReservationIdExists_ReturnsView()
+        public void Delete_ReservationIdExists_ReturnRedirectToActionResult()
         {
             var result = (RedirectToRouteResult)_controller.Delete(_existing);
 
@@ -98,5 +108,23 @@ namespace HotelGarage.UnitTests.Controllers
             Assert.AreEqual("Parking", result.RouteValues["controller"]);
         }
 
+        [Test]
+        public void Save_ViewModelIsValid_ReturnRedirectToActionResult()
+        {
+            var result = (RedirectToRouteResult)_controller.Save(_reservation);
+
+            Assert.AreEqual("Parking", result.RouteValues["action"]);
+            Assert.AreEqual("Parking", result.RouteValues["controller"]);
+        }
+
+        [Test]
+        public void Save_ViewModelIsInValid_ReturnView()
+        {
+            _controller.ModelState.AddModelError("","");
+            var result = _controller.Save(_reservation) as ViewResult;
+
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.ViewName == "Form");
+        }
     }
 }
--- HotelGarage/Controllers/ReservationsController.cs
+++ HotelGarage/Controllers/ReservationsController.cs
@@ -76,8 +76,7 @@ namespace HotelGarage.Controllers
             var reservation = _unitOfWork.Reservations.GetReservation(viewModel.Id);
             var car = _unitOfWork.Cars.GetCar(viewModel);
 
-
-            // vytvoreni auta anebo update
+            // vytvoreni auta anebo update stavajiciho
             if (_unitOfWork.Cars.GetCar(viewModel) == null)
             {
                 car = new Car(viewModel.Car);
@@ -89,37 +88,17 @@ namespace HotelGarage.Controllers
             //vytvoreni rezervace anebo update
             if (viewModel.Id == 0)
             {
-                reservation = new Reservation()
-                {
-                    LicensePlate = viewModel.LicensePlate,
-                    Arrival = viewModel.Arrival,
-                    Departure = viewModel.Departure,
-                    IsRegistered = viewModel.IsRegistered,
-                    ParkingPlaceId = viewModel.ParkingPlaceId,
-                    Car = car,
-                    StateOfReservationId = StateOfReservation.Reserved
-                };
+                reservation = viewModel;
+                reservation.Car = car;
+                reservation.StateOfReservationId = StateOfReservation.Reserved;
 
                 _unitOfWork.Reservations.AddReservation(reservation);
             }
             else
                 reservation.Update(viewModel, car);
 
-            ParkingPlaceNotAssignedAndResNotInhouseCheck(
-                reservation,
-                _unitOfWork.ParkingPlaces.GetParkingPlaceStateOfPlace(reservation),
-                _unitOfWork.StatesOfPlaces);
+            var parkingPlace = _unitOfWork.ParkingPlaces.GetParkingPlaceStateOfPlace(reservation);
 
-            _unitOfWork.Complete();
-
-            return RedirectToAction("Parking", "Parking");
-        }
-
-        public void ParkingPlaceNotAssignedAndResNotInhouseCheck(
-            Reservation reservation, 
-            ParkingPlace parkingPlace, 
-            IStateOfPlaceRepository stateOfPlaceRepository)
-        {
             if (reservation.ParkingPlaceId != 0 && reservation.StateOfReservationId != StateOfReservation.Inhouse)
             {
                 // prirazeni k mistu rezervace a nastaveni mista na rezervovano
@@ -130,6 +109,10 @@ namespace HotelGarage.Controllers
                 else
                     parkingPlace.AssingnFreeParkingPlace(_unitOfWork.StatesOfPlaces.GetFreeStateOfPlace(), reservation);
             }
+
+            _unitOfWork.Complete();
+
+            return RedirectToAction("Parking", "Parking");
         }
     }
 }
